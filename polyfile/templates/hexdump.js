c352cf25a0cc855f1c89745d97ed5525add41d65
Evan Sultanik <evan.sultanik@trailofbits.com>
2019-08-20 17:05:11 -0400

    Genericized the highlighting routine

--- polyfile/templates/hexdump.js
+++ polyfile/templates/hexdump.js
@@ -1,44 +1,28 @@
-var lastHighlight = null;
-var lastHighlightLength = null;
-var lastCursor = null;
-
-function highlight(byte_id, length) {
+function highlight(byte_id, length, css_class) {
     if(typeof length === 'undefined') {
-        length = 1
+        length = 1;
+    }
+    if(typeof css_class === 'undefined') {
+        css_class = "highlighted";
     }
     byte_id = parseInt(byte_id);
     length = parseInt(length);
-    if(lastHighlight !== null) {
-        for(var i=0; i<lastHighlightLength; ++i) {
-            $("#byte" + (lastHighlight + i)).removeClass("highlighted");
-            $("#hexbyte" + (lastHighlight + i)).removeClass("highlighted");
-        }
-    }
+    $("." + css_class).removeClass(css_class);
     for(var i=0; i<length; ++i) {
-        $("#byte" + (byte_id + i)).addClass("highlighted");
-        $("#hexbyte" + (byte_id + i)).addClass("highlighted");
+        $(".byte[byteid=" + (byte_id + i) + "]").addClass(css_class);
     }
-    lastHighlight = byte_id;
-    lastHighlightLength = length;
 }
 
 function cursor(byte_id) {
-    byte_id = parseInt(byte_id);
-    if(lastCursor !== null) {
-        $("#byte" + lastCursor).removeClass("cursor");
-        $("#hexbyte" + lastCursor).removeClass("cursor");
-    }
-    $("#byte" + byte_id).addClass("cursor");
-    $("#hexbyte" + byte_id).addClass("cursor");
-    lastCursor = byte_id;
+    highlight(byte_id, 1, "cursor");
 }
 
 $(document).ready(function() {
     $(".bytes .byte").css("cursor", "none").hover(function() {
-        cursor($(this)[0].id.substring(4));
+        cursor($(this).attr('byteid'));
     })
     $(".hexdump .byte").css("cursor", "none").hover(function() {
-        cursor($(this)[0].id.substring(7));
+        cursor($(this).attr('byteid'));
     })
     $(".tree_label").hover(function() {
         highlight($(this).attr('matchbyte'), $(this).attr('matchlength'));
@@ -51,8 +35,7 @@ $(document).ready(function() {
         }
         var offset = parseInt($(this).attr('matchbyte'));
         for(var i=0; i<parseInt($(this).attr('matchlength')); ++i) {
-            $("#byte" + (offset + i)).addClass("manually-focused");
-            $("#hexbyte" + (offset + i)).addClass("manually-focused");
+            $(".byte[byteid=" + (offset + i) + "]").addClass("manually-focused");
         }
         $(this).addClass("manually-focused");
     });
