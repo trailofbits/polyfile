4a08ade1e2be557144901dfc407e3ebe07a80877
Evan Sultanik <evan.sultanik@trailofbits.com>
2019-08-21 16:02:48 -0400

    Dynamic highlighting

--- polyfile/templates/hexdump.js
+++ polyfile/templates/hexdump.js
@@ -1,7 +1,9 @@
-var rawBytes = btoa("{{ encoded }}");
+var rawBytes = atob("{{ encoded }}");
 var ROWS = Math.max(Math.ceil(rawBytes.length / 16), 1);
 var BYTE_HEIGHT;
 var ROW_OFFSET = 0;
+var VISIBLE_ROWS = 0;
+var highlights = {};
 
 function highlight(byte_id, length, css_class) {
     if(typeof length === 'undefined') {
@@ -12,9 +14,56 @@ function highlight(byte_id, length, css_class) {
     }
     byte_id = parseInt(byte_id);
     length = parseInt(length);
-    $("." + css_class).removeClass(css_class);
+    removeHighlight(css_class);
+    if(!(css_class in highlights)) {
+        highlights[css_class] = [];
+    }
     for(var i=0; i<length; ++i) {
-        $(".byte[byteid=" + (byte_id + i) + "]").addClass(css_class);
+        highlights[css_class].push([-1, byte_id + i]);
+    }
+    updateHighlights(css_class);
+}
+
+function removeHighlight(css_class) {
+    if(!(css_class in highlights)) {
+        return;
+    }
+    for(var i=0; i<highlights[css_class].length; ++i) {
+        var cell_id = highlights[css_class][i][0];
+        var byte_id = highlights[css_class][i][1];
+        $('#byte' + cell_id).removeClass(css_class);
+    }
+    highlights[css_class] = [];
+}
+
+function updateHighlights(css_class) {
+    if(typeof css_class === 'undefined') {
+        /* update all types */
+        for(var css_class in highlights.keys) {
+            updateHighlights(css_class);
+        }
+        return;
+    } else if(!(css_class in highlights)) {
+        return;
+    }
+    var offset = ROW_OFFSET * 16;
+    for(var i=0; i<highlights[css_class].length; ++i) {
+        var cell_id = highlights[css_class][i][0];
+        var byte_id = highlights[css_class][i][1];
+
+        var current_cell_id = byte_id - offset;
+
+        if(current_cell_id != cell_id) {
+            if(cell_id >= 0) {
+                $('#byte' + cell_id).removeClass(css_class);
+                highlights[css_class][i][0] = -1;
+            }
+        }
+
+        if(current_cell_id >= 0 && current_cell_id < VISIBLE_ROWS * 16) {
+            $("#byte" + current_cell_id).addClass(css_class);
+            highlights[css_class][i][0] = current_cell_id;
+        }
     }
 }
 
@@ -22,16 +71,59 @@ function cursor(byte_id) {
     highlight(byte_id, 1, "cursor");
 }
 
-function visibleRows() {
-    return Math.floor($('.hexeditor .scrollcontainer').first().innerHeight() / BYTE_HEIGHT) - 2;
+function scrollToByte(byte_id) {
+    scrollToRow(Math.floor(byte_id / 16));
+}
+
+function scrollToRow(row) {
+    if(typeof row === 'undefined') {
+        row = ROW_OFFSET;
+    }
+    if(row < 0) {
+        row = 0;
+    } else if(row > ROWS - VISIBLE_ROWS) {
+        row = ROWS - VISIBLE_ROWS;
+    }
+    ROW_OFFSET = row;
+    var startOffset = ROW_OFFSET * 16;
+    for(var i=startOffset; i < Math.min(rawBytes.length, startOffset + VISIBLE_ROWS * 16); ++i) {
+        $("#byte" + (i - startOffset)).text(rawBytes.charCodeAt(i).toString(16).padStart(2, '0'));
+    }
+    updateHighlights();
+    $(".hexeditor .scrollcontainer").scrollTop(BYTE_HEIGHT * ROW_OFFSET);
+}
+
+function resizeWindow() {
+    var newVisible = Math.floor($('.hexeditor .scrollcontainer').first().innerHeight() / BYTE_HEIGHT) - 2;
+    while(VISIBLE_ROWS < newVisible) {
+        /* add a new row */
+        var rowHtml = '<div class="byteline"><span class="byterow">'
+            + (VISIBLE_ROWS * 16).toString(16).padStart(5, '0') +
+            '</span>';
+        var offset = VISIBLE_ROWS * 16;
+        for(var i=0; i<16; ++i) {
+            rowHtml += '<span class="byte" id="byte' + (offset + i) + '"></span>';
+        }
+        rowHtml += "</div>";
+        $('.hexdump').append(rowHtml);
+        for(var i=0; i<16; ++i) {
+            var byte_id = offset + i;
+            $("#byte" + byte_id).css("cursor", "none").hover(function() {
+                highlight(parseInt($(this)[0].id.substring(4)) + ROW_OFFSET * 16, 1, "cursor");
+            });
+        }
+        ++VISIBLE_ROWS;
+    }
+    scrollToRow();
 }
 
 $(document).ready(function() {
     BYTE_HEIGHT = $('.hexeditor .byte').first().outerHeight();
     $(".scrollheightproxy").height(BYTE_HEIGHT * ROWS);
-    console.log(BYTE_HEIGHT * ROWS);
+    $(window).resize(resizeWindow());
+    resizeWindow();
     $(".hexeditor .scrollcontainer").scroll(function() {
-        console.log("Scroll!");
+        scrollToRow(Math.floor($(this).scrollTop() / BYTE_HEIGHT));
     });
     $('.hexeditor .scrollcapture').bind('mousewheel', function(e){
         if(e.originalEvent.wheelDelta /120 > 0) {
@@ -39,32 +131,31 @@ $(document).ready(function() {
                 --ROW_OFFSET;
             }
         } else{
-            if(ROW_OFFSET < ROWS - visibleRows() + 1) {
+            if(ROW_OFFSET < ROWS - VISIBLE_ROWS) {
                 ++ROW_OFFSET;
             }
         }
         e.preventDefault();
-        $(".hexeditor .scrollcontainer").scrollTop(BYTE_HEIGHT * ROW_OFFSET);
+        scrollToRow();
     });
-    $(".bytes .byte").css("cursor", "none").hover(function() {
-        cursor($(this).attr('byteid'));
-    })
-    $(".hexdump .byte").css("cursor", "none").hover(function() {
-        cursor($(this).attr('byteid'));
-    })
+//    $(".bytes .byte").css("cursor", "none").hover(function() {
+//        cursor($(this).attr('byteid'));
+//    })
+//    $(".hexdump .byte").css("cursor", "none").hover(function() {
+//        cursor($(this).attr('byteid'));
+//    })
     $(".tree_label").hover(function() {
         highlight($(this).attr('matchbyte'), $(this).attr('matchlength'));
     });
     $(".tree_label:not([for])").css("cursor", "zoom-in").click(function() {
-        var hadFocus = $(this).hasClass("manually-focused");
-        $(".manually-focused").removeClass("manually-focused");
-        if(hadFocus) {
+        if($(this).hasClass("manually-focused")) {
+            removeHighlight("manually-focused");
             return;
         }
+        $(".manually-focused").removeClass("manually-focused");
         var offset = parseInt($(this).attr('matchbyte'));
-        for(var i=0; i<parseInt($(this).attr('matchlength')); ++i) {
-            $(".byte[byteid=" + (offset + i) + "]").addClass("manually-focused");
-        }
+        var length = parseInt($(this).attr('matchlength'));
+        highlight(offset, length, "manually-focused");
         $(this).addClass("manually-focused");
     });
     $(".tree_label").mouseout(function() {
