d0bdf80001af2cd6789508a0eb049335cc7deeca
Evan Sultanik <evan.sultanik@trailofbits.com>
2019-08-26 11:14:18 -0400

    `pdf.PdfMatcher` --> `pdf.PDF`

--- polyfile/pdf.py
+++ polyfile/pdf.py
@@ -22,7 +22,7 @@ def parse_object(object, parent=None):
     log.status('Parsing PDF obj %d %d' % (object.id, object.version))
     objtoken, objid, objversion, endobj = object.objtokens
     pdf_length=endobj.offset.offset - object.content[0].offset.offset + 1 + len(endobj.token)
-    if parent is None or isinstance(parent, PdfMatcher):
+    if parent is None or isinstance(parent, PDF):
         parent_offset = 0
     else:
         parent_offset = parent.offset
