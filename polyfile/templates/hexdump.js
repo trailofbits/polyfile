8e544dfb8e0adb29dd306c365e9a6c782104475f
Evan Sultanik <evan.sultanik@trailofbits.com>
2019-09-06 11:15:02 -0400

    Added a search feature

--- polyfile/templates/hexdump.js
+++ polyfile/templates/hexdump.js
@@ -27,16 +27,21 @@ function assignLines() {
     LINE_DIGITS = Math.ceil(Math.log(linesByRow[linesByRow.length-1]) / Math.log(10));
 }
 
-function highlight(byte_id, length, css_class) {
+function highlight(byte_id, length, css_class, remove_existing) {
     if(typeof length === 'undefined') {
         length = 1;
     }
     if(typeof css_class === 'undefined') {
         css_class = "highlighted";
     }
+    if(typeof remove_existing === 'undefined') {
+        remove_existing = true;
+    }
     byte_id = parseInt(byte_id);
     length = parseInt(length);
-    removeHighlight(css_class);
+    if(remove_existing) {
+        removeHighlight(css_class);
+    }
     if(!(css_class in highlights)) {
         highlights[css_class] = [];
     }
@@ -245,6 +250,115 @@ function clearSelection() {
     }
 }
 
+var searchMatches = [];
+var currentMatch = 0;
+
+function kmp_table(source) {
+    var pos = 0;
+    var cnd = 0;
+    var t = [-1];
+
+    while(++pos < source.length) {
+        if(source.charCodeAt(pos) == source.charCodeAt(cnd)) {
+            t.push(t[cnd]);
+        } else {
+            t.push(cnd);
+            cnd = t[cnd];
+            while(cnd >= 0 && source.charCodeAt(pos) != source.charCodeAt(cnd)) {
+                cnd = t[cnd];
+            }
+        }
+        ++cnd;
+    }
+
+    t[pos] = cnd;
+
+    return t;
+}
+
+function kmp_search(source, find, caseSensitive) {
+    if(!source) {
+        return [];
+    }
+    if(!find) {
+        return source.split('').map(function(_, i) { return i; });
+    }
+    if(typeof caseSensitive === 'undefined') {
+        caseSensitive = false;
+    }
+    if(!caseSensitive) {
+        source = source.toLowerCase();
+        find = find.toLowerCase();
+    }
+    var t = kmp_table(find);
+    var j = 0;
+    var k = 0;
+    var result = [];
+    while(j < source.length) {
+        if(find.charCodeAt(k) == source.charCodeAt(j)) {
+            ++j;
+            if(++k == find.length) {
+                result.push(j - k);
+                k = t[k];
+            }
+        } else {
+            k = t[k];
+            if(k < 0) {
+                ++j;
+                ++k;
+            }
+        }
+    }
+    return result;
+}
+
+function pageSearch() {
+    searchMatches = [];
+    currentMatch = 0;
+    $('#searchinfo').text('');
+    $('#searchbuttons').hide();
+    removeHighlight('searchresult');
+    var query = $('#search').val();
+    if(query === null || query == "") {
+        return;
+    }
+
+    for(var index of kmp_search(rawBytes, query)) {
+        highlight(index, query.length, 'searchresult', false);
+        searchMatches.push(index);
+    }
+
+    if(searchMatches.length == 0) {
+        $('#searchinfo').text('0/0');
+        return;
+    }
+
+    if(searchMatches.length > 1) {
+        $('#searchbuttons').show();
+    }
+
+    updateSearch();
+}
+
+function updateSearch() {
+    $('#searchinfo').text((currentMatch + 1) + '/' + searchMatches.length);
+    scrollToByte(searchMatches[currentMatch]);
+}
+
+function searchUp() {
+    if(--currentMatch < 0) {
+        currentMatch = searchMatches.length - 1;
+    }
+    updateSearch();
+}
+
+function searchDown() {
+    if(++currentMatch >= searchMatches.length) {
+        currentMatch = 0;
+    }
+    updateSearch();
+}
+
 var doubleClicked = false;
 
 $(document).ready(function() {
@@ -298,6 +412,18 @@ $(document).ready(function() {
     });
     $(".tree_label").mouseout(function() {
         $(".highlighted").removeClass("highlighted");
+    });
+
+    $(window).keydown(function(e) {
+        if ( ( e.keyCode == 70 && ( e.ctrlKey || e.metaKey ) ) ||
+            ( e.keyCode == 191 ) ) {
+            e.preventDefault();
+            $('#search').focus().select();
+        } else if ( e.keyCode == 71 && ( e.ctrlKey || e.metaKey ) ) {
+            e.preventDefault();
+            searchDown();
+        }
+        return true;
     });
 
     $(".byterow").css("cursor", "zoom-in").attr('title', 'Double-Click to Jump to Address').dblclick(function() {
