38e7872adc2b2f0fe86e082a826c2556fdd6a542
Evan Sultanik <evan.sultanik@trailofbits.com>
2019-11-23 15:32:23 -0500

    Fix the git branch detection for Travis compatibility

--- polyfile/version.py
+++ polyfile/version.py
@@ -3,13 +3,16 @@ import subprocess
 
 
 def git_branch():
-    branch = subprocess.check_output(
-        ['git', 'symbolic-ref', '-q', 'HEAD'],
-        cwd=os.path.dirname(os.path.realpath(__file__)),
-        stderr=subprocess.DEVNULL
-    )
-    branch = branch.decode('utf-8').strip().split('/')[-1]
-    return branch
+    try:
+        branch = subprocess.check_output(
+            ['git', 'symbolic-ref', '-q', 'HEAD'],
+            cwd=os.path.dirname(os.path.realpath(__file__)),
+            stderr=subprocess.DEVNULL
+        )
+        branch = branch.decode('utf-8').strip().split('/')[-1]
+        return branch
+    except Exception:
+        return None
 
 
 # Change DEV_BUILD to False when deploying to PyPI
@@ -19,7 +22,11 @@ DEV_BUILD = True
 __version__ = (0, 1, 5)
 
 if DEV_BUILD:
-    __version__ = __version__ + ('git', git_branch())
+    branch_name = git_branch()
+    if branch_name is None:
+        __version__ = __version__ + ('git',)
+    else:
+        __version__ = __version__ + ('git', branch_name)
 
 VERSION_STRING = ''
 
