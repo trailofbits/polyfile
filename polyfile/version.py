8aec5e17f53bd23d4100a93051c53c71cd18d5ec
Evan Sultanik <evan.sultanik@trailofbits.com>
2019-11-23 15:26:27 -0500

    Include the git branch in versions strings for dev builds

--- polyfile/version.py
+++ polyfile/version.py
@@ -1,4 +1,25 @@
-__version__ = (0, 1, 4)
+import os
+import subprocess
+
+
+def git_branch():
+    branch = subprocess.check_output(
+        ['git', 'symbolic-ref', '-q', 'HEAD'],
+        cwd=os.path.dirname(os.path.realpath(__file__)),
+        stderr=subprocess.DEVNULL
+    )
+    branch = branch.decode('utf-8').strip().split('/')[-1]
+    return branch
+
+
+# Change DEV_BUILD to False when deploying to PyPI
+DEV_BUILD = True
+
+
+__version__ = (0, 1, 5)
+
+if DEV_BUILD:
+    __version__ = __version__ + ('git', git_branch())
 
 VERSION_STRING = ''
 
@@ -13,3 +34,7 @@ for element in __version__:
             VERSION_STRING += f'-{element!s}'
         else:
             VERSION_STRING += str(element)
+
+
+if __name__ == '__main__':
+    print(VERSION_STRING)
