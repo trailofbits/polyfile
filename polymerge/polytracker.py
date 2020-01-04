7f31dd0643296918fd2866a018c38aece96ad75e
Evan Sultanik <evan.sultanik@trailofbits.com>
2020-01-04 14:13:00 -0500

    Added a missing `__str__` impl

--- polymerge/polytracker.py
+++ polymerge/polytracker.py
@@ -25,6 +25,9 @@ class FunctionInfo:
     def __hash__(self):
         return hash(self.name)
 
+    def __str__(self):
+        return self.name
+
     def __repr__(self):
         return f"{self.__class__.__name__}(name={self.name!r}, input_bytes={self.input_bytes!r}, called_from={self.called_from!r})"
 
