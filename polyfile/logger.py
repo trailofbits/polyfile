c800cc492f9fdde6789ec43af1ad8729957e24a0
Evan Sultanik <evan.sultanik@trailofbits.com>
2020-09-09 16:59:08 -0400

    Do not use `sys.stderr.buffer` or log statuses that are not UTF-8

--- polyfile/logger.py
+++ polyfile/logger.py
@@ -6,22 +6,22 @@ STATUS = 15
 
 
 class StatusLogHandler(logging.StreamHandler):
-    def __init__(self, stream=sys.stderr.buffer):
+    def __init__(self, stream=sys.stderr):
         super().__init__(stream=stream)
-        self._status = b''
+        self._status: str = ''
 
     def emit(self, record):
         isatty = self.stream.isatty()
         if self._status and isatty:
-            self.stream.write(f"\r{' ' * len(self._status)}\r".encode('utf-8'))
+            self.stream.write(f"\r{' ' * len(self._status)}\r")
         msg = record.getMessage()
-        if isinstance(msg, str):
-            msg = msg.encode('utf-8')
+        if isinstance(msg, bytes):
+            msg = msg.decode('utf-8')
         if record.levelno == STATUS and isatty:
             self._status = msg
         else:
             self.stream.write(msg)
-            self.stream.write(b'\n')
+            self.stream.write('\n')
         if self._status and isatty:
             self.stream.write(self._status)
         self.stream.flush()
