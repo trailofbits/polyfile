f5c2421b738b17c6bca258ad0bf4bcefd0153fc1
Evan Sultanik <evan.sultanik@trailofbits.com>
2021-05-13 15:01:38 -0400

    Properly render image previews in HTML again

--- polyfile/kaitaimatcher.py
+++ polyfile/kaitaimatcher.py
@@ -1,14 +1,12 @@
 import base64
 from typing import Dict, Iterator, List, Tuple, Type
 
-from kaitaistruct import KaitaiStructError
+from kaitaistruct import KaitaiStruct, KaitaiStructError
 
-from .kaitai.parser import ASTNode, KaitaiParser, KaitaiStruct, RootNode
+from .kaitai.parser import ASTNode, KaitaiParser, RootNode
 from .kaitai.parsers.gif import Gif
 from .kaitai.parsers.jpeg import Jpeg
-from .kaitai.parsers.pcap import Pcap
 from .kaitai.parsers.png import Png
-from .kaitai.parsers.sqlite3 import Sqlite3
 from .polyfile import submatcher, InvalidMatch, Match, Submatch
 
 
@@ -24,11 +22,13 @@ KAITAI_MIME_MAPPING: Dict[str, str] = {
     "font/sfnt": "font/ttf.ksy"
 }
 
-IMAGE_MIMETYPES: Dict[Type[KaitaiStruct], str] = {
-    Gif: "image/gif",
-    Jpeg: "image/jpeg",
-    Png: "image/png"
+IMAGE_MIMETYPES = {
+    "image/gif",
+    "image/jpeg",
+    "image/png"
 }
+
+MIME_BY_PARSER: Dict[Type[KaitaiStruct], str] = {}
 
 
 def ast_to_matches(ast: RootNode, parent: Match) -> Iterator[Submatch]:
@@ -44,10 +44,11 @@ def ast_to_matches(ast: RootNode, parent: Match) -> Iterator[Submatch]:
             parent=parent
         )
 
-        if node is ast and node.obj.__class__ in IMAGE_MIMETYPES:  # type: ignore
-            # this is an image type, so create a preview
-            new_node.img_data = f"data:{IMAGE_MIMETYPES[node.obj.__class__]};base64," \
-                                f"{base64.b64encode(ast.raw_value).decode('utf-8')}"
+        if node is ast and node.obj.__class__ in MIME_BY_PARSER:  # type: ignore
+            mtype = MIME_BY_PARSER[node.obj.__class__]  # type: ignore
+            if mtype in IMAGE_MIMETYPES:  # type: ignore
+                # this is an image type, so create a preview
+                new_node.img_data = f"data:{mtype};base64,{base64.b64encode(ast.raw_value).decode('utf-8')}"
 
         yield new_node
         stack.extend(reversed([(new_node, c) for c in node.children]))
@@ -64,3 +65,9 @@ for mimetype, kaitai_path in KAITAI_MIME_MAPPING.items():
             except (Exception, KaitaiStructError):
                 raise InvalidMatch()
             yield from ast_to_matches(ast, parent=self)
+
+    MIME_BY_PARSER[KaitaiMatcher.kaitai_parser.struct_type] = mimetype
+
+del mimetype
+del kaitai_path
+del KaitaiMatcher
