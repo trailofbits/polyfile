76f884714dc41f4d639dcc11330626fd3d5b2561
Evan Sultanik <evan.sultanik@trailofbits.com>
2022-11-18 15:09:51 -0500

    Update expected text tests

--- tests/test_magic.py
+++ tests/test_magic.py
@@ -39,43 +39,11 @@ class MagicTest(TestCase):
         for test in matcher.non_text_tests:
             self.assertNotEqual(test.test_type, polyfile.magic.TestType.TEXT)
         num_text_tests = len(matcher.text_tests)
-        expected_text_tests = {'sgml:128', 'c-lang:73', 'misctools:39', 'tex:128', 'commands:137', 'forth:10',
-                               'commands:132', 'tex:74', 'ruby:15', 'tex:91', 'c-lang:90', 'misctools:6', 'sgml:96',
-                               'sisu:8', 'tex:21', 'tex:138', 'tcl:19', 'tex:98', 'tex:134', 'commands:108', 'troff:21',
-                               'tex:116', 'tex:124', 'python:256', 'tex:55', 'tex:87', 'sgml:65', 'tex:88', 'msdos:26',
-                               'ruby:50', 'tex:136', 'c-lang:82', 'ringdove:27', 'c-lang:8', 'python:293', 'gentoo:44',
-                               'sgml:126', 'python:279', 'assembler:15', 'c-lang:69', 'make:15', 'tex:49', 'python:244',
-                               'perl:18', 'tcl:15', 'assembler:9', 'commands:135', 'pascal:5', 'c-lang:60', 'tex:102',
-                               'ringdove:8', 'commands:105', 'ringdove:28', 'sgml:102', 'msx:78', 'sgml:62',
-                               'ringdove:7', 'commands:68', 'linux:507', 'tex:122', 'perl:53', 'c-lang:10',
-                               'commands:102', 'tex:77', 'tex:64', 'linux:54', 'perl:36', 'ringdove:19', 'sisu:14',
-                               'ringdove:22', 'c-lang:33', 'tcl:13', 'inform:9', 'perl:22', 'clojure:26', 'c-lang:25',
-                               'troff:13', 'archive:360', 'tex:106', 'tex:43', 'json:6', 'tex:93', 'fonts:6', 'ruby:37',
-                               'c-lang:51', 'tcl:7', 'windows:325', 'sgml:55', 'tex:71', 'securitycerts:5', 'tcl:25',
-                               'm4:5', 'c-lang:48', 'assembler:5', 'ruby:9', 'c-lang:86', 'tcl:17', 'ruby:25', 'rst:5',
-                               'ringdove:17', 'tex:90', 'commands:111', 'lex:10', 'windows:671', 'ringdove:18',
-                               'lex:12', 'c-lang:29', 'tex:75', 'troff:15', 'make:19', 'troff:19', 'ringdove:32',
-                               'tex:126', 'tex:89', 'sgml:89', 'ringdove:14', 'sisu:17', 'ringdove:25', 'lisp:20',
-                               'tcl:21', 'tex:94', 'ringdove:29', 'uuencode:18', 'tex:46', 'c-lang:78', 'c-lang:65',
-                               'assembler:13', 'sgml:86', 'c-lang:36', 'tex:34', 'm4:8', 'ringdove:12', 'ringdove:9',
-                               'python:263', 'tex:95', 'perl:8', 'tex:76', 'ctags:6', 'audio:631', 'assembler:11',
-                               'make:6', 'securitycerts:4', 'assembler:17', 'sgml:120', 'tex:86', 'perl:12', 'ruby:53',
-                               'lisp:16', 'tex:92', 'fortran:6', 'ringdove:13', 'c-lang:96', 'sgml:127', 'perl:54',
-                               'troff:9', 'nim-lang:7', 'python:250', 'perl:52', 'python:287', 'c-lang:45', 'sgml:74',
-                               'c-lang:39', 'lisp:22', 'lua:11', 'tex:104', 'cad:333', 'ringdove:15', 'sgml:92',
-                               'assembler:7', 'ruby:44', 'tex:118', 'sgml:68', 'linux:174', 'lisp:26', 'fonts:132',
-                               'perl:51', 'perl:40', 'games:286', 'sgml:59', 'tex:132', 'uuencode:26', 'lua:15',
-                               'linux:173', 'sgml:134', 'clojure:23', 'tex:96', 'sisu:5', 'tcl:11', 'tex:36',
-                               'troff:25', 'msdos:28', 'ruby:18', 'tex:58', 'audio:633', 'sgml:49', 'lisp:18',
-                               'perl:49', 'cad:309', 'perl:14', 'lisp:24', 'sketch:6', 'tex:114', 'softquad:26',
-                               'clojure:29', 'lua:13', 'forth:16', 'tex:78', 'lua:9', 'sgml:77', 'ringdove:16',
-                               'tex:112', 'tex:20', 'misctools:44', 'commands:139', 'troff:11', 'tex:67', 'perl:50',
-                               'tex:120', 'sgml:135', 'sgml:136', 'perl:10', 'tex:130', 'ringdove:21', 'ringdove:20',
-                               'tex:100', 'csv:6', 'linux:170', 'perl:48', 'perl:16', 'commands:142', 'sgml:71',
-                               'tex:52', 'c-lang:22', 'ruby:31', 'perl:24', 'bioinformatics:156', 'gnu:170', 'sgml:80',
-                               'terminfo:48', 'tcl:9', 'commands:159', 'ringdove:6', 'ruby:12', 'diff:16',
-                               'subtitle:19', 'psl:9', 'c-lang:42', 'lisp:77', 'perl:20', 'tex:61', 'apple:6', 'tcl:28',
-                               'tex:40', 'sisu:11', 'perl:47', 'ringdove:26', 'sgml:83', 'java:19'}
+        # expected_text_tests = repr({
+        #         f"{test.source_info.path.name}:{test.source_info.line}"
+        #         for test in matcher.text_tests if test.source_info is not None
+        # })
+        expected_text_tests = {'commands:55', 'terminfo:48', 'ruby:53', 'troff:11', 'sgml:59', 'tex:102', 'commands:23', 'commands:61', 'tcl:21', 'sgml:62', 'sgml:74', 'c-lang:48', 'perl:49', 'c-lang:69', 'perl:18', 'clojure:26', 'tex:138', 'tex:118', 'linux:54', 'perl:22', 'lisp:16', 'commands:86', 'sisu:17', 'sisu:14', 'fonts:132', 'c-lang:96', 'perl:12', 'tcl:9', 'troff:19', 'commands:108', 'tex:71', 'perl:48', 'securitycerts:4', 'windows:671', 'tcl:13', 'c-lang:42', 'sgml:136', 'ringdove:7', 'tex:132', 'c-lang:45', 'tex:52', 'tex:104', 'tex:89', 'commands:99', 'commands:49', 'tex:100', 'commands:51', 'assembler:13', 'msx:78', 'tex:112', 'troff:21', 'cad:333', 'misctools:39', 'tex:49', 'gnu:170', 'perl:50', 'commands:47', 'tex:136', 'tcl:25', 'forth:16', 'perl:10', 'perl:53', 'cad:309', 'python:287', 'psl:9', 'python:279', 'commands:14', 'perl:52', 'tcl:17', 'inform:9', 'softquad:26', 'tex:91', 'make:6', 'commands:137', 'troff:15', 'commands:159', 'commands:74', 'tex:64', 'commands:64', 'c-lang:60', 'tex:94', 'assembler:9', 'ringdove:17', 'c-lang:73', 'make:19', 'lex:12', 'make:15', 'python:293', 'ruby:9', 'ringdove:19', 'ringdove:27', 'perl:14', 'commands:66', 'tex:92', 'sgml:126', 'perl:20', 'sgml:49', 'fortran:6', 'ringdove:20', 'c-lang:10', 'sgml:96', 'tex:58', 'subtitle:19', 'uuencode:18', 'c-lang:8', 'c-lang:29', 'csv:6', 'sgml:128', 'tex:46', 'commands:121', 'c-lang:33', 'python:256', 'assembler:15', 'games:286', 'ruby:18', 'tex:76', 'commands:82', 'assembler:5', 'perl:40', 'archive:360', 'json:6', 'commands:40', 'sgml:83', 'linux:174', 'tex:20', 'tex:95', 'sisu:5', 'python:250', 'c-lang:65', 'lua:15', 'commands:7', 'tex:90', 'bioinformatics:156', 'commands:102', 'c-lang:36', 'ringdove:16', 'commands:25', 'tex:106', 'lisp:26', 'ringdove:22', 'gentoo:44', 'commands:139', 'ruby:50', 'commands:18', 'commands:78', 'tex:120', 'ringdove:6', 'tex:114', 'tcl:19', 'mail.news:17', 'c-lang:51', 'commands:53', 'sgml:92', 'troff:9', 'msdos:28', 'forth:10', 'ringdove:28', 'sgml:65', 'commands:132', 'tcl:15', 'ringdove:21', 'ruby:25', 'clojure:23', 'commands:117', 'misctools:6', 'ctags:6', 'lisp:18', 'sgml:135', 'fonts:6', 'audio:631', 'tex:21', 'commands:123', 'sgml:102', 'ringdove:26', 'ringdove:18', 'tex:77', 'sgml:86', 'commands:105', 'tex:86', 'ruby:37', 'sgml:71', 'perl:47', 'tex:122', 'tex:126', 'apple:6', 'tex:128', 'commands:38', 'mail.news:19', 'm4:8', 'tex:96', 'c-lang:86', 'securitycerts:5', 'tcl:28', 'ruby:12', 'c-lang:82', 'uuencode:26', 'ringdove:8', 'tex:40', 'commands:95', 'ringdove:9', 'c-lang:78', 'c-lang:39', 'linux:173', 'commands:43', 'tex:98', 'tex:75', 'tex:43', 'windows:325', 'msdos:26', 'tcl:7', 'commands:97', 'commands:27', 'commands:29', 'troff:25', 'tex:34', 'linux:170', 'nim-lang:7', 'ringdove:15', 'perl:36', 'perl:8', 'tex:87', 'commands:71', 'tex:74', 'commands:34', 'misctools:44', 'commands:57', 'lua:13', 'c-lang:25', 'ringdove:14', 'pascal:5', 'sgml:134', 'ringdove:32', 'lisp:24', 'tex:124', 'perl:51', 'perl:24', 'sgml:80', 'diff:16', 'rst:5', 'commands:135', 'commands:90', 'commands:142', 'lex:10', 'tex:55', 'lua:9', 'tex:36', 'perl:16', 'sgml:55', 'python:263', 'java:19', 'lua:11', 'commands:119', 'lisp:77', 'sisu:11', 'tex:93', 'tex:130', 'troff:13', 'sgml:68', 'ringdove:25', 'tex:67', 'assembler:11', 'c-lang:22', 'ruby:31', 'sgml:127', 'ruby:15', 'ringdove:12', 'sgml:89', 'clojure:29', 'tex:61', 'tex:78', 'linux:507', 'sketch:6', 'commands:68', 'ringdove:29', 'commands:45', 'm4:5', 'commands:111', 'perl:54', 'sisu:8', 'tex:134', 'commands:59', 'ruby:44', 'sgml:120', 'assembler:7', 'audio:633', 'assembler:17', 'c-lang:90', 'tcl:11', 'ringdove:13', 'tex:116', 'python:244', 'sgml:77', 'lisp:22', 'commands:36', 'tex:88', 'lisp:20'}
         if num_text_tests > len(expected_text_tests):
             actual_text_tests = {
                 f"{test.source_info.path.name}:{test.source_info.line}": test
