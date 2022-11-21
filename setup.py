2d19a6c8d6fc938c7a633860efe3b30f55844174
Evan Sultanik <evan.sultanik@trailofbits.com>
2022-11-21 10:44:43 -0500

    Up the version for the next release

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     long_description_content_type="text/markdown",
     url='https://github.com/trailofbits/polyfile',
     author='Trail of Bits',
-    version="0.4.2",
+    version="0.5.0",
     packages=find_packages(exclude=("tests",)),
     python_requires='>=3.7',
     install_requires=[
