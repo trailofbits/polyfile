57a4b343fcd629e96be94ee4456faff8cc5dd1e3
Evan Sultanik <evan.sultanik@trailofbits.com>
2023-11-20 10:35:51 -0500

    Removes official support for Python 3.7

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     author='Trail of Bits',
     version="0.5.3",
     packages=find_packages(exclude=("tests",)),
-    python_requires='>=3.7',
+    python_requires='>=3.8',
     install_requires=[
         "abnf~=2.2.0",
         "chardet~=5.0.0",
